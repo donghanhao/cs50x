1adb56b296a25de86eb8b00e41602323b4cf8dde
donghanhao <104446375+donghanhao@users.noreply.github.com>
2022-10-11 00:43:40 +0800

    Add files via upload

---
+++ C/pset1/mario-less.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height:");
+    }
+    while (n < 1 || n > 8);
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = n - i - 1; k > 0; k--)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
